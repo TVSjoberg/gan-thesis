80396e04661f542460a62d4b343e6218899bdb10
TVSjoberg <t.sjoberg@sterna.se>
2020-03-31 15:01:35 +0200

    quick fix of df_concat_ind

--- evaluation/pMSE.py
+++ evaluation/pMSE.py
@@ -2,6 +2,7 @@ import sys
 import os
 sys.path.append(
     os.path.join(os.path.dirname(__file__), os.path.pardir))
+
 import numpy as np
 import pandas as pd
 import sklearn.linear_model
@@ -17,6 +18,7 @@ def pMSE(dataframe, ind_var):
     pmse = (sum((prediction[:,1] - 0.5)**2))/len(dataframe)
     return pmse
     
+
 def null_pmse_est(dataframe, ind_var, n_iter):
     ##Randomly assigns the indicator variable
     df = dataframe.copy()
@@ -34,21 +36,10 @@ def df_concat_ind(real_df, gen_df, ind = 'ind'):
     #help function to set up the indicator function
     r_df = real_df.copy()
     g_df = gen_df.copy()
-    df = pd.concat((r_df, gen_df), axis=0)
+    df = pd.concat((r_df, gen_df), axis=0).reset_index(drop=True)
     
     ind_col = pd.DataFrame(np.ones( (len(df), 1) ) , columns= [ind])
+    ind_col.iloc[len(r_df):,0] = 0;
     df = pd.concat((df, ind_col), axis = 1)
     return df
-
-    
-# def testfun():
-#     testing_df, info_1 = multivariate_df(100, [0,0], np.eye(2))
-#     df2, info_2 = multivariate_df(100, [1,1], [[1,0.5],[0.5,1]])
-#     testing_df = pd.concat([testing_df, pd.DataFrame(np.ones(len(testing_df)), columns = ['ind'])], axis = 1) 
-#     df2 = pd.concat([df2, pd.DataFrame(np.zeros(len(df2)), columns = ['ind'])], axis = 1) 
-#     df = pd.concat([testing_df, df2])
-#     df = df.reset_index(drop=True)
-
-#     print(null_pmse_est(df, 'ind', n_iter = 100))
-#     print(pMSE(df, 'ind'))
 
