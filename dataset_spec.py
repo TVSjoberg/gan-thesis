13d1e00a1ae82e3080156350a5edfaf321e7f0e0
TVSjoberg <t.sjoberg@sterna.se>
2020-04-29 13:46:27 +0200

    new data specifications

--- dataset_spec.py
+++ dataset_spec.py
@@ -7,12 +7,13 @@ rint = np.random.randint
 
 seed = 123
 np.random.seed(seed)
+n_samples = 10000
 
 mvn_test1 = {
     # 3 INDEPENDENT features
     # 1 standard normal, 1 high mean, 1 high var
     #
-    'n_samples': 10000,
+    'n_samples': n_samples,
     'mean': [0, 3, 0],
     'var': [1, 1, 5],
     'corr': np.eye(3).tolist(),
@@ -23,7 +24,7 @@ mvn_test2 = {
     # medium positive
     # medium negative
     # correlations
-    'n_samples': 10000,
+    'n_samples': n_samples,
     'mean': [0, 0, 0],
     'var': [1, 1, 1],
     'corr': [[1, 0.3, -0.3],
@@ -34,7 +35,7 @@ mvn_test2 = {
 
 
 mvn_test3 = {
-    'n_samples': 10000,
+    'n_samples': n_samples,
     'mean': unif(-3, 3, 9).tolist(),
     'var': unif(0, 1, 9).tolist(),
     'corr': r_corr(9).tolist(),
@@ -55,7 +56,7 @@ ln_test3 = mvn_test3.copy()
 #rand_prop = (rand_prop/sum(rand_prop))
 
 mvn_mix_test1 = {
-    'n_samples': 10000,
+    'n_samples': n_samples,
     'proportions':
         [0.05, 0.10, 0.35, 0.5],
 
@@ -94,7 +95,7 @@ mvn_mix_test1 = {
 
 size = 3
 mvn_mix_test2 = {
-    'n_samples': 10000,
+    'n_samples': n_samples,
     'proportions': rand_prop(3),
     'means': [
         unif(0, 8, size).tolist() for i in range(3)
@@ -114,7 +115,7 @@ mvn_mix_test2 = {
 # ln COPY AV MVN
 
 cat_test1 = {
-    'n_samples': 10000,
+    'n_samples': n_samples,
     'probabilities': [
         [0.5, 0.5],
         [0.1, 0.2, 0.7],
@@ -125,7 +126,7 @@ cat_test1 = {
 }
 
 cond_cat_test1 = {
-    'n_samples': 10000,
+    'n_samples': n_samples,
     'ind_probs': [
         [1/2, 3/8, 1/8]
     ],
@@ -152,7 +153,7 @@ n_lab_cond = [rint(3, 6, n_cond[i]) for i in range(n_ind)]
 n_labs = n_lab_ind.tolist()+list(chain(*n_lab_cond))
 
 cond_cat_test2 = {
-    'n_samples' : 10000,
+    'n_samples' : n_samples,
     'ind_probs' : [
         rand_prop(size).tolist() for size in n_lab_ind
     ],
@@ -166,7 +167,7 @@ cond_cat_test2 = {
 
 
 gauss_mix_cond_test1 = {
-    'n_samples': 10000,
+    'n_samples': n_samples,
     'ind_probs': [[1/3, 1/3, 1/3]],
     'means': [
         [
@@ -206,7 +207,7 @@ n_cont_per_cat = [2 for i in (n_labs)]
 
 
 gauss_mix_cond_test2 = {
-    'n_samples' : 10000,
+    'n_samples' : n_samples,
     'ind_probs' : [
         rand_prop(size).tolist() for size in n_lab_ind
     ],
@@ -254,7 +255,7 @@ n_cont_per_cat = [5 for i in (n_labs)]
 
 
 gauss_mix_cond_test3 = {
-    'n_samples' : 10000,
+    'n_samples' : n_samples,
     'ind_probs' : [
         rand_prop(size).tolist() for size in n_lab_ind
     ],
